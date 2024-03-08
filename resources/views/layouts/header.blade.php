8e89070656c2d24826e9932c7cfdb51a2cac9ae1
Jeyps29 <jpcaballes69@gmail.com>
2024-03-08 15:19:15 +0800

    secondcommit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,39 +8,21 @@
             <div class="spinner-border Typeahead-spinner" role="status"><span class="sr-only">Loading...</span></div><i class="close-search" data-feather="x"></i>
           </div>
           <div class="Typeahead-menu"></div>
-        </div>
+        </div>  
       </div>
     </form>
     <div class="header-logo-wrapper col-auto p-0">
       <div class="logo-wrapper"><a href="{{ route('index')}}"><img class="img-fluid" src="{{ asset('assets/images/logo/logo.png') }}" alt=""></a></div>
       <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="align-center"></i></div>
     </div>
-    <div class="left-header col-xxl-5 col-xl-6 col-lg-5 col-md-4 col-sm-3 p-0">
-      <div class="notification-slider">
-        <div class="d-flex h-100"> <img src="{{ asset('assets/images/giftools.gif') }}" alt="gif">
-          <h6 class="mb-0 f-w-400"><span class="font-primary">Don't Miss Out! </span><span class="f-light">Out new update has been release.</span></h6><i class="icon-arrow-top-right f-light"></i>
-        </div>
-        <div class="d-flex h-100"><img src="{{ asset('assets/images/giftools.gif') }}" alt="gif">
-          <h6 class="mb-0 f-w-400"><span class="f-light">Something you love is now on sale! </span></h6><a class="ms-1" href="https://1.envato.market/3GVzd" target="_blank">Buy now !</a>
-        </div>
-      </div>
-    </div>
+   
     <div class="nav-right col-xxl-7 col-xl-6 col-md-7 col-8 pull-right right-header p-0 ms-auto">
       <ul class="nav-menus">
         <li class="language-nav">
           <div class="translate_wrapper">
             <div class="current_lang">
-              <div class="lang"><i class="flag-icon flag-icon-us"></i><span class="lang-txt">EN                               </span></div>
-            </div>
-            <div class="more_lang">
-              <div class="lang selected" data-value="en"><i class="flag-icon flag-icon-us"></i><span class="lang-txt">English<span> (US)</span></span></div>
-              <div class="lang" data-value="de"><i class="flag-icon flag-icon-de"></i><span class="lang-txt">Deutsch</span></div>
-              <div class="lang" data-value="es"><i class="flag-icon flag-icon-es"></i><span class="lang-txt">Español</span></div>
-              <div class="lang" data-value="fr"><i class="flag-icon flag-icon-fr"></i><span class="lang-txt">Français</span></div>
-              <div class="lang" data-value="pt"><i class="flag-icon flag-icon-pt"></i><span class="lang-txt">Português<span> (BR)</span></span></div>
-              <div class="lang" data-value="cn"><i class="flag-icon flag-icon-cn"></i><span class="lang-txt">简体中文</span></div>
-              <div class="lang" data-value="ae"><i class="flag-icon flag-icon-ae"></i><span class="lang-txt">لعربية <span> (ae)</span></span></div>
-            </div>
+            
+           
           </div>
         </li>
         <li>                         <span class="header-search">
@@ -100,52 +82,8 @@
             </svg>
           </div>
         </li>
-        <li class="cart-nav onhover-dropdown">
-          <div class="cart-box">
-            <svg>
-              <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-ecommerce') }}"></use>
-            </svg><span class="badge rounded-pill badge-success">2</span>
-          </div>
-          <div class="cart-dropdown onhover-show-div">
-            <h6 class="f-18 mb-0 dropdown-title">Cart</h6>
-            <ul>
-              <li>
-                <div class="media"><img class="img-fluid b-r-5 me-3 img-60" src="{{ asset('assets/images/other-images/cart-img.jpg') }}" alt="">
-                  <div class="media-body"><span>Furniture Chair for Home</span>
-                    <div class="qty-box">
-                      <div class="input-group"><span class="input-group-prepend">
-                          <button class="btn quantity-left-minus" type="button" data-type="minus" data-field="">-</button></span>
-                        <input class="form-control input-number" type="text" name="quantity" value="1"><span class="input-group-prepend">
-                          <button class="btn quantity-right-plus" type="button" data-type="plus" data-field="">+</button></span>
-                      </div>
-                    </div>
-                    <h6 class="font-primary">$500</h6>
-                  </div>
-                  <div class="close-circle"><a class="bg-danger" href="#"><i data-feather="x"></i></a></div>
-                </div>
-              </li>
-              <li>
-                <div class="media"><img class="img-fluid b-r-5 me-3 img-60" src="{{ asset('assets/images/other-images/cart-img.jpg') }}" alt="">
-                  <div class="media-body"><span>Furniture Chair for Home</span>
-                    <div class="qty-box">
-                      <div class="input-group"><span class="input-group-prepend">
-                          <button class="btn quantity-left-minus" type="button" data-type="minus" data-field="">-</button></span>
-                        <input class="form-control input-number" type="text" name="quantity" value="1"><span class="input-group-prepend">
-                          <button class="btn quantity-right-plus" type="button" data-type="plus" data-field="">+</button></span>
-                      </div>
-                    </div>
-                    <h6 class="font-primary">$500.00</h6>
-                  </div>
-                  <div class="close-circle"><a class="bg-danger" href="#"><i data-feather="x"></i></a></div>
-                </div>
-              </li>
-              <li class="total">
-                <h6 class="mb-0">Order Total : <span class="f-right">$1000.00</span></h6>
-              </li>
-              <li class="text-center"><a class="d-block mb-3 view-cart f-w-700" href="#">Go to your cart</a><a class="btn btn-primary view-checkout" href="#">Checkout</a></li>
-            </ul>
-          </div>
-        </li>
+      
+        
         <li class="onhover-dropdown">
           <div class="notification-box">
             <svg>
