86d08c69ff2bd8d55b1c4dbdc027c88d53f143d3
nyaps12 <raffylimbs17@gmail.com>
2024-03-08 13:40:44 +0800

    route

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class AdminController extends Controller
+{
+    //
+    public function dashboard()
+    {
+        return view('index');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function LoginAdmin(Request $request)
+    {
+        $user = user()->id();
+
+        return redirect()->route('index', compact('user'));
+
+
+        ///SET UP LOGIN NATIN MAMA
+    }
+}
